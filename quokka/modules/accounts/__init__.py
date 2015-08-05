89cf5456c328cd4abe8920ca4793e5177041ecd7
felipevolpone <felipevolpone@gmail.com>
2015-08-05 19:34:24 -0300

    Deleting unecessary view from accounts and changing the register_user to include recaptcha

--- quokka/modules/accounts/__init__.py
+++ quokka/modules/accounts/__init__.py
@@ -1,7 +1,3 @@
 # coding: utf8
 from quokka.core.app import QuokkaModule
-from .views import AccountView
-
 module = QuokkaModule('accounts', __name__, template_folder='templates')
-module.add_url_rule('/accounts/new/',
-                    view_func=AccountView.as_view('accounts'))
